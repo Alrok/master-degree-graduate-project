aafbded0008a6cec0c0e3f60a1ca93173492eca4
Rokytskyi <o.rokytskyi@spheremall.com>
2019-09-22 01:44:40 +0300

    Clean docker configuration

---
+++ shop-app/app/Facades/Elasticsearch.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1roky
+ * Date: 22.09.2019
+ * Time: 1:32
+ */
+
+namespace App\Facades;
+
+
+class Elasticsearch
+{
+
+}
---
+++ shop-app/app/Http/Controllers/Catalog/CatalogController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1roky
+ * Date: 22.09.2019
+ * Time: 0:03
+ */
+
+namespace App\Http\Controllers\Catalog;
+
+
+class CatalogController
+{
+
+}
