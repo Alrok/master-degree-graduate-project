bc01b6de13ca117d4c69f3466ded797d243a1a42
Oleksandr Rokytskyi <1rokytskyy@gmail.com>
2019-09-29 20:08:19 +0300

    Use computing service do receive products

--- shop-app/app/Http/Controllers/Catalog/CatalogController.php
+++ shop-app/app/Http/Controllers/Catalog/CatalogController.php
@@ -18,19 +18,9 @@ class CatalogController extends Controller
     public function index()
     {
         $client = new Client();
-        $res = $client->request('GET',env('COMPUTING_SERVICE_URL'));
-        $json = json_decode($res->getBody()->getContents(), true);
+        $res = $client->request('GET', env('COMPUTING_SERVICE_URL').'/product');
+        $products = json_decode($res->getBody()->getContents(), true);
 
-
-        $return = Elasticsearch::search(['index' => 'products']);
-
-        $products = [];
-
-        foreach ($return['hits']['hits'] ?? [] as $product) {
-            $products[$product['_id']] = $product['_source'];
-            $products[$product['_id']]['_id'] = $product['_id'];
-        }
-
-        return view('catalog', ['products' => $products]);
+        return view('catalog', ['products' => $products['items']]);
     }
 }
